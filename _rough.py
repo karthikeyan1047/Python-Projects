d32dca79f7024ec1e017664f1bdfab6c2d45470f
karthikeyan1047 <karthikeyan61709497@gmail.com>
2025-09-12 11:48:58 +0530

    changes1

--- _rough.py
+++ _rough.py
@@ -1,8 +1,1 @@
-import pandas as pd
-
-path = r"c:\Users\karthikeyans\Documents\Epicle\Audit File\Audit Raw Files\Audit Master File  - Aug'2025- Week-4 1.xlsx"
-
-df = pd.read_excel(path, sheet_name='Prebilling Data')
-dd = df.iloc[15,5]
-print(dd)
 
